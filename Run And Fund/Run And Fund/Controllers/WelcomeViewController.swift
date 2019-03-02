6a52dfdafbdf7cb9197d0104e62b78aa66411eb6
Vikas Shetty <vikasrs4u@gmail.com>
2019-03-02 14:25:15 -0800

    Added code to keep user logged in even when user quits the app.

--- Run And Fund/Run And Fund/Controllers/WelcomeViewController.swift
+++ Run And Fund/Run And Fund/Controllers/WelcomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class WelcomeViewController: UIViewController
 {
@@ -14,7 +15,27 @@ class WelcomeViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Below code is added to keep user logged in even when user quits the app.
+        let currentUser = Auth.auth().currentUser
+        
+        Auth.auth().addStateDidChangeListener
+            {
+                (auth, user) in
+                
+                if (user == currentUser && user != nil)
+                {
+                    self.performSegueFunc()
+                }
+                
+        }
+
+
+    }
+    
+    func performSegueFunc()
+    {
+        self.performSegue(withIdentifier:"goToGetStarted" , sender: self)
     }
 
 
