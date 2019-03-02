b5dccb157dc80608b2e5693d08ed15010553e3a7
Vikas Shetty <vikasrs4u@gmail.com>
2019-03-02 12:11:26 -0800

    Added signup view controller and also refactored the code to proper file groups

---
+++ Run And Fund/Run And Fund/Controllers/SignUpViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  Run And Fund
+//
+//  Created by Vikas R S on 3/2/19.
+//  Copyright © 2019 Vikas Radhakrishna Shetty. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
