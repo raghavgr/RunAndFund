72f182c69a71ed29136dd3b71b0423b848098586
Vikas Shetty <vikasrs4u@gmail.com>
2019-03-02 15:34:47 -0800

    Complete the design and logic handling for Login screen

---
+++ Run And Fund/Run And Fund/Controllers/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  Run And Fund
+//
+//  Created by Vikas R S on 3/2/19.
+//  Copyright © 2019 Vikas Radhakrishna Shetty. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class LoginViewController: UIViewController
+{
+
+    @IBOutlet var emailIDFieldOutlet: UITextField!
+    
+    @IBOutlet var passwordField: UITextField!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginButtonIsClicked(_ sender: UIButton)
+    {
+        SVProgressHUD.show()
+        
+        Auth.auth().signIn(withEmail: emailIDFieldOutlet.text!, password: passwordField.text!)
+        { (result, error) in
+            
+            if(error != nil)
+            {
+                let errorMessage:String = (error?.localizedDescription.description)!
+                
+                SVProgressHUD.showError(withStatus:errorMessage)
+                
+            }
+            else
+            {
+                SVProgressHUD.showSuccess(withStatus: "Sucessfully Logged In")
+                self.performSegue(withIdentifier:"goToGetStarted", sender: self)
+            }
+        }
+    }
+    
+}
