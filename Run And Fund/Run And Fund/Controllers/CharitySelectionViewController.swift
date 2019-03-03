d1ffb78cb27599ceb78cc01af160cad222dec83c
Vikas Shetty <vikasrs4u@gmail.com>
2019-03-02 16:19:21 -0800

    Added UI screen and Controller for Charity Selection

---
+++ Run And Fund/Run And Fund/Controllers/CharitySelectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CharitySelectionViewController.swift
+//  Run And Fund
+//
+//  Created by Vikas R S on 3/2/19.
+//  Copyright © 2019 Vikas Radhakrishna Shetty. All rights reserved.
+//
+
+import UIKit
+
+class CharitySelectionViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
