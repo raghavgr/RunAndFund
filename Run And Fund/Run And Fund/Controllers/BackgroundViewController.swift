4483776317952e7ec6bea08df4c966b259a1911b
Sai Grandhi <raghavgr94@gmail.com>
2019-03-02 23:42:43 -0800

    :mount_fuji: Background color functionality refactored

---
+++ Run And Fund/Run And Fund/Controllers/BackgroundViewController.swift
@@ -0,0 +1,50 @@
+//
+//  BackgroundViewController.swift
+//  Run And Fund
+//
+//  Created by Sai Grandhi on 3/2/19.
+//  Copyright © 2019 Vikas Radhakrishna Shetty. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addGradientToView(view: self.view)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    func addGradientToView(view: UIView)
+    {
+        //gradient layer
+        let gradientLayer = CAGradientLayer()
+        
+        //define colors
+        let colorTop = UIColor(red: 255.0 / 255.0, green: 226.0 / 255.0, blue: 89.0 / 255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0 / 255.0, green: 167.0 / 255.0, blue: 81.0 / 255.0, alpha: 1.0).cgColor
+        gradientLayer.colors = [colorTop, colorBottom]
+        
+        //define locations of colors as NSNumbers in range from 0.0 to 1.0
+        //if locations not provided the colors will spread evenly
+        //gradientLayer.locations = [0.5, 0.6, 0.8]
+        
+        //define frame
+        gradientLayer.frame = view.bounds
+        gradientLayer.zPosition = -1
+        //insert the gradient layer to the view layer
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
--- Run And Fund/Run And Fund/Controllers/WelcomeViewController.swift
+++ Run And Fund/Run And Fund/Controllers/WelcomeViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 import Firebase
 import SVProgressHUD
 
-class WelcomeViewController: UIViewController
+class WelcomeViewController: BackgroundViewController
 {
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        addGradientToView(view: self.view)
         
         // image icon
         let imageName = "icon-left-font-monochrome-black"
@@ -55,26 +54,6 @@ class WelcomeViewController: UIViewController
         self.performSegue(withIdentifier:"goToGetStarted" , sender: self)
     }
 
-    func addGradientToView(view: UIView)
-    {
-        //gradient layer
-        let gradientLayer = CAGradientLayer()
-        
-        //define colors
-        let colorTop = UIColor(red: 255.0 / 255.0, green: 226.0 / 255.0, blue: 89.0 / 255.0, alpha: 1.0).cgColor
-        let colorBottom = UIColor(red: 255.0 / 255.0, green: 167.0 / 255.0, blue: 81.0 / 255.0, alpha: 1.0).cgColor
-        gradientLayer.colors = [colorTop, colorBottom]
-        
-        //define locations of colors as NSNumbers in range from 0.0 to 1.0
-        //if locations not provided the colors will spread evenly
-        //gradientLayer.locations = [0.5, 0.6, 0.8]
-        
-        //define frame
-        gradientLayer.frame = view.bounds
-        gradientLayer.zPosition = -1
-        //insert the gradient layer to the view layer
-        view.layer.insertSublayer(gradientLayer, at: 0)
-    }
 
 }
 
