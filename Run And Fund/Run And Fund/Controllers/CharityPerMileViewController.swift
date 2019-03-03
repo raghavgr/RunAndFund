9065e350043b79a0553a423defea76d88a9d5367
Vikas Shetty <vikasrs4u@gmail.com>
2019-03-02 23:43:39 -0800

    Made changes for charity picker

--- Run And Fund/Run And Fund/Controllers/CharityPerMileViewController.swift
+++ Run And Fund/Run And Fund/Controllers/CharityPerMileViewController.swift
@@ -11,12 +11,16 @@ import Firebase
 
 class CharityPerMileViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource
 {
-        let currencyNameArray = ["2"]
+
+    
+        let charityPerMileArray = ["$0.25/mile","$0.5/mile","$1.00/mile", "$1.25/mile", "$1.50/mile", "$1.75/mile","$2.00/mile", "$2.50/mile","$3.00/mile", "$3.50/mile","$4.00/mile","$4.50/mile","$5.00/mile"]
     
 
     @IBOutlet var userNameLabel: UILabel!
     
     var charitySelected: String?
+    
+    var charityAmountSelected:String = ""
     
     @IBOutlet var charityNameOutlet: UILabel!
     
@@ -30,6 +34,9 @@ class CharityPerMileViewController: UIViewController, UIPickerViewDelegate,UIPic
         
         charityNameOutlet.text = charitySelected!
         // Do any additional setup after loading the view.
+        
+        charityPickerOutlet.dataSource = self
+        charityPickerOutlet.delegate = self
     }
     
 
@@ -40,14 +47,31 @@ class CharityPerMileViewController: UIViewController, UIPickerViewDelegate,UIPic
         userNameLabel.text = "Hello, \(String(describing: name))"
     }
     
+    //MARK: - Picker Data Source methods
     
-   // Picker data source method
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return charityPerMileArray.count
     }
+    
+    //MARK: - Picker Delegate methods
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return charityPerMileArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        charityAmountSelected = charityPerMileArray[row]
+    }
+    
+    
+
 
 }
